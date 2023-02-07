4c2deeb037351a8ab015bf027819b42e2d9ef7fa
Grace Little <gracevlittle@gmail.com>
2023-02-07 13:58:24 -0600

    everything but the expel

--- main.js
+++ main.js
@@ -18,16 +18,23 @@ const studentsArray = [
     house: "Slytherin",
   },
 ];
-
-
-const hogwartsHouses = ['Gryffindor', 'Hufflepugg', 'Slytherin', 'Ravenclaw'];
-const housedStudents = [];
+const hogwartsHouses = ['Gryffindor', 'Hufflepuff', 'Slytherin', 'Ravenclaw'];
 const expelledStudents = [];
 
+
+//render to DOM utility function 
+const renderToDom = (divId, htmlToRender) => {
+  const selectedDiv = document.querySelector(divId);
+  
+  selectedDiv.innerHTML = htmlToRender;
+};
+
+//WELCOME PAGE
 const sortingHat = document.getElementById("sorting-hat");
 const targetDiv = document.getElementById("hidden-div");
-const btn = document.getElementById("get-startedbtn");
-btn.onclick = function () {
+const getStartedBtn = document.getElementById("get-startedbtn");
+// function to hide+show divs on click. CHANGE TO FAT ARROW FUNCTION
+getStartedBtn.onclick = function () {
   if (targetDiv.style.display = "none") {
     targetDiv.style.display = "block";
   }
@@ -38,32 +45,39 @@ btn.onclick = function () {
   }
 }
 
-
-
+// get cards on the DOM
 const cardsOnDom = (array) => {
   // define variable to retrieve strings
   let domString = "";
-  // open array using .foreach to access objects
-  array.forEach(function (singleStudentObjects) {
-  // take empty domString variable and add code to it
-  domString = 
-    domString +
-    `
-    <article>
-      <div class="card" style="width: 18rem;">
-      <h4>${singleStudentObjects.name}</h4>
-      <p>${singleStudentObjects.specialSkill}</p>
-      <p>${singleStudentObjects.house}</p>
-      </div>
-    </article>
-    `;
-  })
-  // let studentCards be = to that div
-  const studentCards = document.querySelector(".first-years");
-  // reder cards to Dom 
-  studentCards.innerHTML = domString;
+  for (const item of array) {
+    domString += 
+    `<div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">${item.name}</h5>
+      <h5>${item.house}</h5>
+    </div>
+    <button class="btn btn-danger" id="expel--${item.id}">Expel</button>
+  </div>`
+  }
+
+ renderToDom("#first-years", domString);
+
 };
 
+// render dark side on the DOM
+const voldyOnDom = (array) => {
+  let domString = "";
+  for (const student of array) {
+    domString += 
+    `<div class="card mb-3" style="width: 14rem;">
+      <div class="card-body">
+        <h5 class="card-tite">Sadly, ${student.name} has moved to the dark side</h5>
+      </div>
+    </div>`
+  }
+
+  renderToDom("#thedarkside", domString);
+};
 
 // function to filter students with specific house
 const filter = (studentsArray, typeString) => {
@@ -103,3 +117,54 @@ showSlytherinButton.addEventListener("click", () => {
   const slytherinHouse = filter(studentsArray, "Slytherin");
   cardsOnDom(slytherinHouse);
 });
+
+//function to create student
+const createStudent = (event) => {
+  event.preventDefault();
+  //grab name from the form
+  const studentName = document.getElementById("student");
+  console.log(studentName);
+  console.log(studentName.value);
+  // create an object from values 
+  const newStudent = {
+    name: studentName.value,
+    house: hogwartsHouses[Math.floor(Math.random()*hogwartsHouses.length)],
+  };
+  console.log("new student", newStudent);
+  //push to students array 
+  studentsArray.push(newStudent);
+
+  //rerender with new array
+  cardsOnDom(studentsArray);
+};
+
+//Sort button
+const sortButton = document.querySelector("#form-submit");
+sortButton.addEventListener("click", createStudent);
+
+// Expel button
+const expelStudent = (e) => {
+  //if id includes expell button
+  if (e.target.id.includes("expelButton")) {
+    // get that object id off of the target ID
+    const [, studentId] = e.target.id.split("--");
+    // use it to find the index of the object 
+    const studentIndex = team.findIndex (
+      (student) => Number(studentId) === student.id
+      );
+      const expelledStudent = studentsArray.splice(studentIndex, 1);
+      
+      expelledStudents.push(expelledStudent);
+
+      voldyOnDom(expelledStudent);
+      cardsOnDom(studentsArray);
+  }
+}
+
+
+
+const startApp = () => {
+  cardsOnDom(studentsArray);
+};
+
+startApp();
